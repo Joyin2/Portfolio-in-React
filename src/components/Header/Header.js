e4ffae6b4a1ee3f6dc0d2965b41ee1fb1b4bec91
Joyin2 <joyinlaskar2000@gmail.com>
2023-02-24 23:59:19 +0530

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,45 +1,29 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
 
 const Header = () => {
     
   return (
-    <>
-      <Navbar bg="light" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
-            </Nav>
-          </Navbar.Collapse>
-          {/* <div id="mobile">
-            <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
-          </div> */}
-        </Container>
-      </Navbar>
-    </>
+    <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Joyin</div>
+        <span>toggle</span>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul>
+            <li>Home</li>
+            <li>Services</li>
+            <li>Exprience</li>
+            <li>Portfolio</li>
+            <li>Testimonials</li>
+          </ul>
+        </div>
+        <button className="button">
+          Contact us
+        </button>
+      </div>
+    </div>
   );
 };
 
