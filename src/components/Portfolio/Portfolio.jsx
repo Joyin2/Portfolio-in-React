ff601bba2a6d8002f14a35c65f3a626ea7da5b89
Joyin2 <joyinlaskar2000@gmail.com>
2023-07-13 10:06:09 +0530

    projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,16 +9,25 @@ import MusicApp from "../../images/musicapp.png"
 import "swiper/css"
 import { themeContext } from '../../Context'
 import { useContext } from 'react'
+import { Link } from 'react-router-dom';
+
+import BulkChoice from "../../images/project-image/bulk-choice.png"
+import Color from "../../images/project-image/color.png"
+import Md from "../../images/project-image/md-sil.png"
+import ToDo from "../../images/project-image/to-do.png"
+import UkpNew from "../../images/project-image/ukp-new.png"
+import UkpOld from "../../images/project-image/ukp-old.png"
+import Diosc from "../../images/project-image/dioscport.png"
 
 
 SwiperCore.use([Navigation, Pagination]);
 const Portfolio = () => {
   const breakpoints = {
     480: {
-      slidesPerView: 1.8,
+      slidesPerView: 1,
     },
     768: {
-      slidesPerView: 2,
+      slidesPerView: 1.5,
     },
     1024: {
       slidesPerView: 3,
@@ -43,16 +52,26 @@ const Portfolio = () => {
         className="portfolio-slider"
       >
         <SwiperSlide>
-          <img src={Sidebar} alt="" />
+          <a href="https://bulkchoice.netlify.app"><img src={BulkChoice} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={Ecommerce} alt="" />
+          <a href="https://ukpschool.in/"><img src={UkpOld} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={MusicApp} alt="" />
+          <a href="https://dioscportt.netlify.app/"><img src={Diosc} alt="" /></a>
         </SwiperSlide>
         <SwiperSlide>
-          <img src={HOC} alt="" />
+          <a href="https://randomcolorgenerator001.netlify.app"><img src={Color} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="https://mdsil.netlify.app"><img src={Md} alt="" /></a>
+        </SwiperSlide>
+        <SwiperSlide>
+          <a href="https://jtodo-crud.netlify.app"><img src={ToDo} alt="" /></a>
+        </SwiperSlide>
+        
+        <SwiperSlide>
+          <a href="https://ukpschoolreact.netlify.app"><img src={UkpNew} alt="" /></a>
         </SwiperSlide>
       </Swiper>
     </div>
